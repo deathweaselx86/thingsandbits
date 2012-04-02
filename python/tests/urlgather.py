73ba2652ce15073524e8049bf981ce35710c4cd7
Jessica Ross <deathweasel@gmail.com>
2012-04-02 15:12:00 -0500

    Adding this header so I don't have to think about 4 spaces versus tabs.

--- python/tests/urlgather.py
+++ python/tests/urlgather.py
@@ -1,3 +1,7 @@
+#/usr/bin/env python
+# -*- coding: utf-8; mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vim: fileencoding=utf-8 tabstop=4 expandtab shiftwidth=4
+
 '''
 This module holds the URLGatherer class and associated exceptions.
 URLGatherer is a primitive webcrawler.
