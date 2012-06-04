f37615d3d1231568d4b8d49b0ff19d6d7c06e2b9
Jessica Ross <deathweasel@gmail.com>
2012-06-03 21:55:04 -0500

    Realized limitations of standard 9V alkaline battery, so adding some power saving code to this. Also abandoning the idea of transmitting data via Processing.

--- c/arduino/weather_station_v2/weather_station_v2.ino
+++ c/arduino/weather_station_v2/weather_station_v2.ino
@@ -1,19 +1,26 @@
 /*
 
 This is an IEEE 802.15.4 based weather monitor sketch.
-It's really just half of the solution; the other half will be
-a Processing sketch that will intercept the messages and
-send them to Cosm.
+It's really just half of the solution.
+
+I decided to use another Arduino to send the data to Cosm
+rather than spend the time to figure out how to cast a generic
+bitstream to int in Processing. Lazy, sorry. I have other things
+I want to do with the same time.
 
 Jessica Ross
 deathweasel@gmail.com
 
 Thank you to ladyada, nethoncho et al for the DHT22 
-code!
+code! Thanks to the xbee-arduino guys, too.
+
 */
 
 #include <DHT.h>
 #include <XBee.h>
+#include <avr/interrupt.h>
+#include <avr/power.h>
+#include <avr/sleep.h>
 
 #define DHTTYPE DHT22
 
@@ -26,30 +33,59 @@ DHT dht(DHTPIN, DHTTYPE);
 // Allocate 4 bytes of data for each sensor value passed.
 byte payload[] = {0,0,0,0,0,0,0,0};
 XBee xbee = XBee();
-XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x408632CB);
-TxStatusResponse txStatus = TxStatusResponse();
-Tx64Request tx64 = Tx64Request(addr64, payload, sizeof(payload));
+Tx16Request tx16 = Tx16Request(0x0, payload, sizeof(payload));
 
 
-float humidity, temp;
-String data;
+volatile float humidity, temp;
 
 void setup()
 {
-  pinMode(8,OUTPUT);
-  pinMode(9,OUTPUT);
+ // XBee and sensor setup.
   xbee.begin(9600);
   dht.begin();
-  delay(5000);
-
+  pinMode(DHTPIN, INPUT);
+  
+  // Set up  Timer 1 for ISR that actually sends data so
+  // we can maybe shut some things down for a few seconds
+  // while we wait.
+  cli(); // global interrupts off
+  // TCCR1A and TCCR1B are reggisters that control Timer1
+  // Set both to zero for bit operations.
+  TCCR1A = 0;
+  TCCR1B = 0; 
+  
+  TIMSK1 = (1 << TOIE1); // enable Timer1 overflow interrupt  
+  // Set 1024 timer prescaler, which gets us something in 
+  // the vicinity of 4 s. I wish we could get something longer
+  // than this.
+  // Ideal would be 10 s. 
+  TCCR1B |= (1 << CS10);
+  TCCR1B |= (1 << CS12);
+  sei(); // global interrupts on
 }
+
 
 void loop()
 {
+  
+  // Set up sleep mode.
+  set_sleep_mode(SLEEP_MODE_IDLE);
+  sleep_enable();
+  // Actually sleep.
+  sleep_mode();
+  // We continue here after the interrupt.
+  // Sleep mode disabled in interrupt.
+
+}
+
+ISR(TIMER1_OVF_vect)
+{
+    // Turn things on!
+    sleep_disable();
     temp = dht.readTemperature();
     humidity = dht.readHumidity();
     sendData(temp,humidity);
-    delay(2000);
+    // Shut things down!
 }
 
 void sendData(float temp, float humidity)
@@ -59,43 +95,23 @@ void sendData(float temp, float humidity)
     // this so much and yet I can't accomplish what I want
     // without it.
     
-    digitalWrite(8,LOW);
-    digitalWrite(9,LOW);
-    delay(5000);
     for(int i=0;i<sizeof(payload); i++)
       payload[i] = 0;
     int * tempPtr = (int *) (& temp);
     int * humidityPtr = (int *) (& humidity);
     //pack temp
-    payload[0] = (*tempPtr >> 24) & 0xff;
-    payload[1] = (*tempPtr >> 16) & 0xff;
-    payload[2] = (*tempPtr >> 8) & 0xff;
-    payload[3] = (*tempPtr) & 0xff;
+    payload[0] = *tempPtr >> 24 & 0xff;
+    payload[1] = *tempPtr >> 16 & 0xff;
+    payload[2] = *tempPtr >> 8 & 0xff;
+    payload[3] = *tempPtr & 0xff;
     //pack humidity
-    payload[4] = (*humidityPtr >> 24) & 0xff;
-    payload[5] = (*humidityPtr >> 16) & 0xff;
-    payload[6] = (*humidityPtr >> 8) & 0xff;
-    payload[7] = (*humidityPtr) & 0xff;
+    payload[4] = *humidityPtr >> 24 & 0xff;
+    payload[5] = *humidityPtr >> 16 & 0xff;
+    payload[6] = *humidityPtr >> 8 & 0xff;
+    payload[7] = *humidityPtr & 0xff;
    
-    xbee.send(tx64);
-    digitalWrite(9,HIGH);
+    xbee.send(tx16);
+    // I don't care about the other XBee's response, so
+    // why bother?    
     
-    // Wait for the other XBee to let us know it got something.
-    if (xbee.readPacket(5000)) {
-        if (xbee.getResponse().getApiId() == TX_STATUS_RESPONSE) {
-           xbee.getResponse().getZBTxStatusResponse(txStatus);
-           if (txStatus.getStatus() == SUCCESS) {
-               // Great. What should we do to denote success?
-               digitalWrite(8,HIGH); 
-                
-           } else {
-                // the other XBee did not receive our packet. What do
-                // we do to denote failute?
-                ;
-                          }
-        }      
-    } else {
-      // Did we miss the status response???
-      ;
-    }
 }
