6a34c169790b91375b58fd3bde431b13b8dd3a6d
Jessica Ross <deathweasel@gmail.com>
2012-06-24 18:10:59 -0500

    This is probably the final version of this sketch. Cleans up some misleading comments and Serial debug statements.

--- c/arduino/weather_txn/weather_txn.ino
+++ c/arduino/weather_txn/weather_txn.ino
@@ -1,7 +1,7 @@
 /*
 This sketch belongs to the other half of an
 IEEE 802.15.4-based weather monitor solution.
-It sends data received to Cosm.com.
+It sends data received 
 */
 
 #include <Ethernet.h>
@@ -9,7 +9,7 @@ It sends data received to Cosm.com.
 #include <stdio.h>
 #include <XBee.h>
 
-#define PACHUBEKEY "redacted"
+#define PACHUBEKEY "uEiSX0-LtHYwtDXVlDT0AgalaP2SAKx2MWtWVExvK0Uxcz0g"
 #define USERAGENT "New Arduino Feed"
 #define FEEDID "59148"
 
@@ -19,7 +19,6 @@ XBeeResponse response = XBeeResponse();
 // create reusable response objects for responses we expect to handle 
 Rx16Response rx16 = Rx16Response();
 
-const int postingInterval = 5000; // Send data every 3600 seconds
 uint8_t payload[] = {0,0,0,0,0,0,0,0};
 
 
@@ -40,7 +39,6 @@ void setup()
   // If Ethernet fails, then don't bother with the contents of
   // the loop() function.
   if (Ethernet.begin(mac) == 0){
-     Serial.println("Failed to get IP via DHCP");
      for(;;)
       ; 
   }
@@ -53,33 +51,24 @@ void loop()
     int temp, humidity;
     temp = 0;
     humidity = 0;
-    Serial.println("beginning of loop");
-    xbee.readPacket();
-    if (xbee.getResponse().isAvailable()) { // There is data.
-      if (xbee.getResponse().getApiId() == RX_16_RESPONSE) { // And it's the type we expect.
+    
+    if (xbee.readPacket(225000)) { // There is data.
           xbee.getResponse().getRx16Response(rx16);
            for(int i=0;i<8;i++)
              payload[i] = rx16.getData(i);
-        }
+        
     
-      //unpack temp
-      temp =  temp | (payload[0] << 8);
-      temp = temp | payload[1];
+          //unpack temp
+          temp =  temp | (payload[0] << 8);
+          temp = temp | payload[1];
 
-      //unpack humidity
-      humidity = humidity | (payload[2] >> 8);
-      humidity = humidity | payload[3];
-  
-      for(int i=0;i<4;i++)
-         Serial.println(payload[i]);
-      // These values are both out of sensor range.
-      // I'll use these to log an error condition for now.
+          //unpack humidity
+          humidity = humidity | (payload[2] >> 8);
+          humidity = humidity | payload[3];
      
-      data = constructCsvString(float(temp)/10, float(humidity)/10);
-      Serial.println(data);
-      sendData(data);
-      }
-      delay(postingInterval);
+          data = constructCsvString(float(temp)/10, float(humidity)/10);
+          sendData(data);
+    }
  
 }
  
@@ -110,37 +99,27 @@ void loop()
  
  void sendData(String csvString)
  {
-
-     if (client.connect(server,80)) {
-         Serial.println("Connecting to cosm");
+         if (client.connect(server, 80)) {
+           client.print("PUT /v2/feeds/");
+           client.print(FEEDID);
+           client.println(".csv HTTP/1.1");
+           client.println("Host: api.cosm.com");
+           client.print("X-ApiKey: ");
+           client.println(PACHUBEKEY);
+           client.print("User-Agent: ");
+           client.println(USERAGENT);
          
-         client.print("PUT /v2/feeds/");
-         client.print(FEEDID);
-         client.println(".csv HTTP/1.1");
-         client.println("Host: api.cosm.com");
-         client.print("X-ApiKey: ");
-         client.println(PACHUBEKEY);
-         client.print("User-Agent: ");
-         client.println(USERAGENT);
-         
-         // I get a 401 Authentication 401 Unauthorized response without this
-         // Something tells me that's an issue that Cosm needs to hammer out.
-         //client.println("Authorization: Basic ZGVhdGh3ZWFzZWw6bmFuZHk4OA=="); 
-         client.print("Content-Length: ");
-         client.println(csvString.length()); 
-         client.println("Content-Type: text/csv");
-         client.println("Connection: close");
-         client.println();
+        
+           client.print("Content-Length: ");
+           client.println(csvString.length()); 
+           client.println("Content-Type: text/csv");
+           client.println("Connection: close");
+           client.println();
 
-         client.println(csvString);
-         client.stop();
-     }
-     else {
-        Serial.println("Connection failed.");
-        Serial.println("Disconnecting");
-        client.stop();
-     }
-     
+           client.println(csvString);
+           client.stop();
+         }
+         
   }
   
 
