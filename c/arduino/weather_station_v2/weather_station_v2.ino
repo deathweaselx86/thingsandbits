1fc7cec22213357ded08db36008ffc5ab11bb388
Jessica Ross <deathweasel@gmail.com>
2012-06-24 18:12:27 -0500

    Probably the final version of this sketch. Fixed sleep; it will sleep about 4 s at a time and send a payload to the other XBee about every 15 minutes.

--- c/arduino/weather_station_v2/weather_station_v2.ino
+++ c/arduino/weather_station_v2/weather_station_v2.ino
@@ -21,12 +21,16 @@ code! Thanks to the xbee-arduino guys, too.
 #include <avr/interrupt.h>
 #include <avr/power.h>
 #include <avr/sleep.h>
+#include <SoftwareSerial.h>
 
 #define DHTTYPE DHT22
 
 // Sensor information.
 // DHT22 sensor is set up on digital pin 7.
 const int DHTPIN=7;
+const int XBEE_SLEEP_PIN = 9;
+const int LOG_TIME = 255;
+
 DHT dht(DHTPIN, DHTTYPE);
 
 // XBee information.
@@ -38,29 +42,35 @@ Tx16Request tx16 = Tx16Request(0x0, payload, sizeof(payload));
 
 volatile float humidity, temp;
 volatile int humidity_i, temp_i;
+volatile int timer_variable = 0;
+
+
 void setup()
 {
  // XBee and sensor setup.
   xbee.begin(9600);
   dht.begin();
-  pinMode(DHTPIN, INPUT);
   
+  pinMode(DHTPIN, INPUT);
+  pinMode(XBEE_SLEEP_PIN, OUTPUT);
+  // Initially, the XBee sleeps.
+  digitalWrite(XBEE_SLEEP_PIN, HIGH); 
   // Set up  Timer 1 for ISR that actually sends data so
   // we can maybe shut some things down for a few seconds
   // while we wait.
   cli(); // global interrupts off
-  // TCCR1A and TCCR1B are reggisters that control Timer1
+  // TCCR1A and TCCR1B are registers that control Timer1
   // Set both to zero for bit operations.
   TCCR1A = 0;
   TCCR1B = 0; 
   
   TIMSK1 = (1 << TOIE1); // enable Timer1 overflow interrupt  
+  
   // Set 1024 timer prescaler, which gets us something in 
   // the vicinity of 4 s. I wish we could get something longer
   // than this.
   // Ideal would be 10 s. 
-  TCCR1B |= (1 << CS10);
-  TCCR1B |= (1 << CS12);
+  TCCR1B |= (1 << CS10) | (1 << CS12);
   sei(); // global interrupts on
 }
 
@@ -83,11 +93,21 @@ void loop()
 ISR(TIMER1_OVF_vect)
 {
 
-    temp = dht.readTemperature();
-    humidity = dht.readHumidity();
-    temp_i = int(temp*10);
-    humidity_i = int(humidity*10);
-    sendData(temp_i,humidity_i);
+    // We set this up such that the contents
+    // of this if statement should execute every 15 minutes.
+    timer_variable +=1;
+    if (timer_variable == LOG_TIME)
+    {
+      digitalWrite(XBEE_SLEEP_PIN, LOW);
+      delay(30); // Delay to let it wake up from advanced sleep.
+      temp = dht.readTemperature();
+      humidity = dht.readHumidity();
+      temp_i = int(temp*10);
+      humidity_i = int(humidity*10);
+      sendData(temp_i,humidity_i);
+      digitalWrite(XBEE_SLEEP_PIN, HIGH);
+      timer_variable = 0;
+    }
 }
 
 void sendData(int temp_i, int humidity_i )
@@ -102,6 +122,7 @@ void sendData(int temp_i, int humidity_i )
    
     xbee.send(tx16);
     // I don't care about the other XBee's response, so
-    // why bother?    
+    // why bother?
+ 
     
 }
