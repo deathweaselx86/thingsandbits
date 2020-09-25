d24bc0d4a169e1f53aac7d9287c128970136b80f
Jessica Ross <jessica.ross@optimizely.com>
2020-09-25 14:33:27 -0500

    there we go

--- leetcode/circular_queue.py
+++ leetcode/circular_queue.py
@@ -11,7 +11,7 @@ class MyCircularQueue:
         Initialize your data structure here. Set the size of the queue to be k.
         """
         self.max_size = k
-        self.current_capacity = 0
+        self.current_size = 0
         self.rear_index = 0
         self.front_index = -1 # set this to one so we can always increment when we add a new element
         self.circular_queue = [None] * k
@@ -20,21 +20,22 @@ class MyCircularQueue:
         """
         Insert an element into the circular queue. Return true if the operation is successful.
         """
+
         if not self.isFull():
             self.front_index = (self.front_index + 1) % self.max_size
             self.circular_queue[self.front_index] = value
-            self.current_capacity = self.current_capacity + 1
+            self.current_size = self.current_size + 1
             return True
         return False
 
-            
+
     def deQueue(self) -> bool:
         """
         Delete an element from the circular queue. Return true if the operation is successful.
         """
         if not self.isEmpty():
             self.circular_queue[self.rear_index] = None
-            self.current_capacity = self.current_capacity - 1
+            self.current_size = self.current_size - 1
             self.rear_index = (self.rear_index + 1) % self.max_size
             return True
         return False
@@ -44,7 +45,7 @@ class MyCircularQueue:
         Get the front item from the queue.
         """
         if not self.isEmpty():
-            return self.circular_queue[self.front_index]
+            return self.circular_queue[self.rear_index]
         return -1
 
     def Rear(self) -> int:
@@ -52,17 +53,17 @@ class MyCircularQueue:
         Get the last item from the queue.
         """
         if not self.isEmpty():
-            return self.circular_queue[self.rear_index]
+            return self.circular_queue[self.front_index]
         return -1
 
     def isEmpty(self) -> bool:
         """
         Checks whether the circular queue is empty or not.
         """
-        return self.current_capacity < 1
+        return self.current_size < 1
 
     def isFull(self) -> bool:
         """
         Checks whether the circular queue is full or not.
         """
-        return self.current_capacity == self.max_size
+        return self.current_size == self.max_size
